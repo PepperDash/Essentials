b1742675c28963528f0d895df5278531345511cf
Neil Dorin <ndorin@pepperdash.com>
2017-10-31 11:57:01 -0600

    Prep for merge into development for NYU deployment

---
+++ Essentials Core/PepperDashEssentialsBase/Crestron IO/Inputs/IDigitalInput.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace PepperDash.Essentials.Core.Crestron_IO
+{
+    public interface IDigitalInput
+    {
+
+    }
+}
---
+++ Essentials Core/PepperDashEssentialsBase/Crestron IO/Relay/GenericRelayDevice.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+using Crestron.SimplSharpPro;
+
+namespace PepperDash.Essentials.Core.Crestron_IO
+{
+    /// <summary>
+    /// Represents a generic device controlled by relays
+    /// </summary>
+    public class GenericRelayDevice
+    {
+        //Relay RelayOutput { get; private set; }
+
+        //public BoolFeedback RelayStateFeedback { get; private set; }
+
+        //Func<bool> RelayStateFeedbackFunc
+        //{
+        //    get
+        //    {
+        //        return () => RelayOutput.State;
+        //    }
+        //}
+
+        //public GenericRelayDevice(Relay relay)
+        //{
+        //    RelayStateFeedback = new BoolFeedback(RelayStateFeedbackFunc);
+
+        //    if(relay.AvailableForUse)
+        //        RelayOutput = relay;
+
+        //    RelayOutput.StateChange += new RelayEventHandler(RelayOutput_StateChange);
+        //}
+
+        //void RelayOutput_StateChange(Relay relay, RelayEventArgs args)
+        //{
+        //    RelayStateFeedback.FireUpdate();
+        //}
+    }
+}
