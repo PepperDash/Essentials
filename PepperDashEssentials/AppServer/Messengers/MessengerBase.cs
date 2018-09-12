ee6293fa7511347ab3b9c83517872241bd565e7e
Heath Volmer <hvolmer@pepperdash.com>
2018-09-12 13:53:35 -0600

    Added MessengerBase class to app server stuff

---
+++ PepperDashEssentials/AppServer/Messengers/MessengerBase.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+using PepperDash.Essentials.Devices.Common.Codec;
+using PepperDash.Essentials.Devices.Common.VideoCodec;
+
+namespace PepperDash.Essentials.AppServer.Messengers
+{
+	/// <summary>
+	/// Provides a messaging bridge for a VideoCodecBase
+	/// </summary>
+	public abstract class MessengerBase
+	{
+		/// <summary>
+		/// 
+		/// </summary>
+		public CotijaSystemController AppServerController { get; private set; }
+
+		public string MessagePath { get; private set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="codec"></param>
+		public MessengerBase(string messagePath)
+		{
+			if (string.IsNullOrEmpty(messagePath))
+				throw new ArgumentException("messagePath must not be empty or null");
+
+			MessagePath = messagePath;
+		}
+		
+
+		/// <summary>
+		/// Registers this messenger with appserver controller
+		/// </summary>
+		/// <param name="appServerController"></param>
+		public void RegisterWithAppServer(CotijaSystemController appServerController)
+		{
+			if (appServerController == null)
+				throw new ArgumentNullException("appServerController");
+			
+			AppServerController = appServerController;
+			CustomRegisterWithAppServer(AppServerController);
+		}
+
+		/// <summary>
+		/// Implemented in extending classes. Wire up API calls and feedback here
+		/// </summary>
+		/// <param name="appServerController"></param>
+		abstract protected void CustomRegisterWithAppServer(CotijaSystemController appServerController);
+
+		/// <summary>
+		/// Helper for posting status message
+		/// </summary>
+		/// <param name="contentObject">The contents of the content object</param>
+		protected void PostStatusMessage(object contentObject)
+		{
+			AppServerController.SendMessageToServer(JObject.FromObject(new
+			{
+				type = MessagePath,
+				content = contentObject
+			}));
+		}
+	}
+}
