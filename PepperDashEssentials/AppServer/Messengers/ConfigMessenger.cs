038e23289eeb1d37275de3863687ea42b2df31fb
Neil Dorin <ndorin@pepperdash.com>
2019-03-12 11:08:32 -0600

    Adds inital ConfigMessenger

---
+++ PepperDashEssentials/AppServer/Messengers/ConfigMessenger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace PepperDash.Essentials.AppServer.Messengers
+{
+    public class ConfigMessenger
+    {
+    }
+}
