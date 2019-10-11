5b05e8bb488365826e8287a55b9530727b25f4bf
Neil Dorin <ndorin@pepperdash.com>
2019-10-11 10:07:35 -0600

    Adds config object for NDisplayRoom

---
+++ PepperDashEssentials/Room/Config/EssentialsNDisplayRoomPropertiesConfig.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace PepperDash.Essentials.Room.Config
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class EssentialsNDisplayRoomPropertiesConfig : EssentialsRoomPropertiesConfig
+    {
+        public string DefaultAudioBehavior { get; set; }
+        public string DefaultAudioKey { get; set; }
+        public string DefaultVideoBehavior { get; set; }
+        public Dictionary<string, string> Displays { get; set; }
+        public string SourceListKey { get; set; }
+
+        public EssentialsNDisplayRoomPropertiesConfig()
+        {
+            DisplayKeys = new List<string>();
+        }
+    }
+}
