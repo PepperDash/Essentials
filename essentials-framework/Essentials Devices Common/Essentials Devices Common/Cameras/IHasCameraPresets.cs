3b84c0e3dbf7a89f6ae94b56b5926c869b27513c
Neil Dorin <ndorin@pepperdash.com>
2019-11-26 14:20:28 -0700

    Adds PresetBase class and new IHasCameraPresets interface

---
+++ essentials-framework/Essentials Devices Common/Essentials Devices Common/Cameras/IHasCameraPresets.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+
+namespace PepperDash.Essentials.Devices.Common.Cameras
+{
+    /// <summary>
+    /// Describes a camera with preset functionality
+    /// </summary>
+    public interface IHasCameraPresets
+    {
+        event EventHandler<EventArgs> PresetsListHasChanged;
+
+        List<CameraPreset> Presets { get; }
+
+        void PresetSelect(int preset);
+
+        void PresetStore(int preset, string description);
+    }
+}
