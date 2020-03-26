a2ca41d965836185e8f18d2b1b88822c17364d9c
Neil Dorin <ndorin@pepperdash.com>
2020-03-26 11:59:32 -0600

    Adds back in the plugin interfaces.  Modifies PluginLoader to try new inteface method for custom plugins first, then fail back to legacy method

--- essentials-framework/Essentials Core/PepperDashEssentialsBase/Plugins/IPluginDeviceConfig.cs
+++ essentials-framework/Essentials Core/PepperDashEssentialsBase/Plugins/IPluginDeviceConfig.cs
@@ -3,9 +3,13 @@ using PepperDash.Essentials.Core.Config;
 
 namespace PepperDash.Essentials.Core.Plugins
 {
+    /// <summary>
+    /// Defines a class that is capable of loading custom plugin device types
+    /// </summary>
     public interface IPluginDeviceConfig
     {
         string MinimumEssentialsFrameworkVersion { get; }
+        void LoadPlugin();
         IKeyed BuildDevice(DeviceConfig dc);
     }
 }
