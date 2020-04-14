33f5881e91935c7cfc100a5f7fda2e1df8c4d071
Andrew Welker <andrew.welker@gmail.com>
2020-04-14 16:17:11 -0600

    Changes base class and adds LinkToApi Method

--- essentials-framework/Essentials Core/PepperDashEssentialsBase/Crestron IO/Relay/GenericRelayDevice.cs
+++ essentials-framework/Essentials Core/PepperDashEssentialsBase/Crestron IO/Relay/GenericRelayDevice.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Text;
 using Crestron.SimplSharp;
 using Crestron.SimplSharpPro;
-
+using Crestron.SimplSharpPro.DeviceSupport;
+using Newtonsoft.Json;
 using PepperDash.Core;
+using PepperDash.Essentials.Core.Bridges;
+using PepperDash_Essentials_Core.Devices;
 
 namespace PepperDash.Essentials.Core.CrestronIO
 {
     /// <summary>
     /// Represents a generic device controlled by relays
     /// </summary>
-    public class GenericRelayDevice : Device, ISwitchedOutput
+    public class GenericRelayDevice : EssentialsBridgeableDevice, ISwitchedOutput
     {
         public Relay RelayOutput { get; private set; }
 
@@ -65,5 +68,37 @@ namespace PepperDash.Essentials.Core.CrestronIO
         }
 
         #endregion
+
+        public override void LinkToApi(BasicTriList trilist, uint joinStart, string joinMapKey, EiscApi bridge)
+        {
+            var joinMap = new GenericRelayControllerJoinMap();
+
+            var joinMapSerialized = JoinMapHelper.GetSerializedJoinMapForDevice(joinMapKey);
+
+            if (!string.IsNullOrEmpty(joinMapSerialized))
+                joinMap = JsonConvert.DeserializeObject<GenericRelayControllerJoinMap>(joinMapSerialized);
+
+            joinMap.OffsetJoinNumbers(joinStart);
+
+            if (RelayOutput == null)
+            {
+                Debug.Console(1, this, "Unable to link device '{0}'.  Relay is null", Key);
+                return;
+            }
+
+            Debug.Console(1, this, "Linking to Trilist '{0}'", trilist.ID.ToString("X"));
+
+            trilist.SetBoolSigAction(joinMap.Relay, b =>
+            {
+                if (b)
+                    CloseRelay();
+                else
+                    OpenRelay();
+            });
+
+            // feedback for relay state
+
+            OutputIsOnFeedback.LinkInputSig(trilist.BooleanInput[joinMap.Relay]);
+        }
     }
 }
