b70739ac4c342a99ab8215dae7aac8c1bca9a4f8
Jason T Alborough <jtalborough@gmail.com>
2020-04-16 14:54:46 -0400

    Adds a reference for IBridge back into PepperDash.Essentials.Bridges that inherits from PepperDash.Essentials.Core.Bridges.IBridge for legacy plugins

---
+++ PepperDashEssentials/Bridges/IBridge.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Crestron.SimplSharpPro.DeviceSupport;
+using PepperDash.Essentials.Core.Bridges;
+
+namespace PepperDash.Essentials.Bridges
+{
+    /// <summary>
+    /// Defines a device that uses the legacy JoinMapBase for its join map
+    /// </summary>
+    [Obsolete("IBridgeAdvanced should be used going forward with JoinMapBaseAdvanced")]
+    public interface IBridge : PepperDash.Essentials.Core.Bridges.IBridge
+    {
+        
+    }
+
+}
