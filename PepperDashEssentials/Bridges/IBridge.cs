072618f981daefb9f37a2c41aa34065ad7d4c829
Andrew Welker <andrew.welker@gmail.com>
2020-04-20 10:12:33 -0600

    moves IBridge back to Essentials

--- PepperDashEssentials/Bridges/IBridge.cs
+++ PepperDashEssentials/Bridges/IBridge.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Crestron.SimplSharpPro.DeviceSupport;
 
 namespace PepperDash.Essentials.Bridges
 {
@@ -6,7 +7,8 @@ namespace PepperDash.Essentials.Bridges
     /// Defines a device that uses the legacy JoinMapBase for its join map
     /// </summary>
     [Obsolete("IBridgeAdvanced should be used going forward with JoinMapBaseAdvanced")]
-    public interface IBridge: Core.Bridges.IBridge
+    public interface IBridge
     {
+        void LinkToApi(BasicTriList trilist, uint joinStart, string joinMapKey);
     }
 }
