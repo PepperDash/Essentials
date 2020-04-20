44793f037533a06b53506b91bf9261fc50abfa9b
Andrew Welker <andrew.welker@gmail.com>
2020-04-20 10:11:35 -0600

    removes IBridge from Essentials Core

--- essentials-framework/Essentials Core/PepperDashEssentialsBase/Bridges/IBridge.cs
+++ essentials-framework/Essentials Core/PepperDashEssentialsBase/Bridges/IBridge.cs
@@ -8,15 +8,6 @@ namespace PepperDash.Essentials.Core.Bridges
     /// </summary>
     public interface IBridgeAdvanced
     {
-        void LinkToApi(BasicTriList trilist, uint joinStart, string joinMapKey, EiscApi bridge);
-    }
-
-    /// <summary>
-    /// Defines a device that uses the legacy JoinMapBase for its join map
-    /// </summary>
-    [Obsolete("IBridgeAdvanced should be used going forward with JoinMapBaseAdvanced")]
-    public interface IBridge
-    {
-        void LinkToApi(BasicTriList trilist, uint joinStart, string joinMapKey);
+        void LinkToApi(BasicTriList trilist, uint joinStart, string joinMapKey, EiscApiAdvanced bridge);
     }
 }
