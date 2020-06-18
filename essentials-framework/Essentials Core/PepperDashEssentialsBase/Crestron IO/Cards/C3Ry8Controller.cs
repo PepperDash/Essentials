1bd682525883c122d94ff104cb0d5d10740d0913
Andrew Welker <andrew.welker@gmail.com>
2020-06-17 23:17:45 -0600

    create classes for all supported cards

---
+++ essentials-framework/Essentials Core/PepperDashEssentialsBase/Crestron IO/Cards/C3Ry8Controller.cs
@@ -0,0 +1,29 @@
+﻿using Crestron.SimplSharpPro;
+using Crestron.SimplSharpPro.ThreeSeriesCards;
+
+namespace PepperDash.Essentials.Core.CrestronIO.Cards
+{
+    public class C3Ry8Controller:C3CardControllerBase, IRelayPorts
+    {
+        private readonly C3ry8 _card;
+
+        public C3Ry8Controller(string key, string name, C3ry8 hardware) : base(key, name, hardware)
+        {
+            _card = hardware
+        }
+
+        #region Implementation of IRelayPorts
+
+        public CrestronCollection<Relay> RelayPorts
+        {
+            get { return _card.RelayPorts; }
+        }
+
+        public int NumberOfRelayPorts
+        {
+            get { return _card.NumberOfRelayPorts; }
+        }
+
+        #endregion
+    }
+}
