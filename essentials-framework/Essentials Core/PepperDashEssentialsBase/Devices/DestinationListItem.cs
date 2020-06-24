f83682960f5cfc945fb706693b2b0aaedc83a151
Andrew Welker <andrew.welker@gmail.com>
2020-06-24 16:39:25 -0600

    update config with DestinationList

--- essentials-framework/Essentials Core/PepperDashEssentialsBase/Devices/DestinationListItem.cs
+++ essentials-framework/Essentials Core/PepperDashEssentialsBase/Devices/DestinationListItem.cs
@@ -1,23 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Crestron.SimplSharp;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using PepperDash.Essentials.Core;
 
 namespace PepperDash_Essentials_Core.Devices
 {
     public class DestinationListItem
     {
-        [JsonProperty("sinkYey")]
+        [JsonProperty("sinkKey")]
         public string SinkKey { get; set; }
 
         private EssentialsDevice _sinkDevice;
 
+        [JsonIgnore]
         public EssentialsDevice SinkDevice
         {
             get { return _sinkDevice ?? (_sinkDevice = DeviceManager.GetDeviceForKey(SinkKey) as EssentialsDevice); }
         }
+
+        public string PreferredName
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(Name))
+                {
+                    return Name;
+                }
+
+                return SinkDevice == null ? "---" : SinkDevice.Name;
+            }
+        }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("includeInDestinationList")]
+        public bool IncludeInDestinationList { get; set; }
+
+        [JsonProperty("order")]
+        public int Order { get; set; }
+
+        [JsonProperty("surfaceLocation")]
+        public int SurfaceLocation { get; set; }
+
+        [JsonProperty("verticalLocation")]
+        public int VerticalLocation { get; set; }
+        
+        [JsonProperty("horizontalLocation")]
+        public int HorizontalLocation { get; set; }
     }
 }
