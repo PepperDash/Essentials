4db91b4f6841c837869ec5135a90234c9b4a4104
Andrew Welker <andrew.welker@gmail.com>
2020-07-14 09:50:25 -0600

    Inheritance

--- PepperDashEssentials/UIDrivers/IHasCalendarButton.cs
+++ PepperDashEssentials/UIDrivers/IHasCalendarButton.cs
@@ -1,16 +1,14 @@
-﻿using PepperDash.Essentials.Core;
-
-namespace PepperDash.Essentials
+﻿namespace PepperDash.Essentials
 {
     public interface IHasCalendarButton
     {
-        EssentialsRoomBase CurrentRoom { get; }
+        EssentialsHuddleVtc1Room CurrentRoom { get; }
         void CalendarPress();
     }
 
     public interface IHasCallButton
     {
-        EssentialsRoomBase CurrentRoom { get; }
+        EssentialsHuddleVtc1Room CurrentRoom { get; }
         void ShowActiveCallsList();
     }
 }
