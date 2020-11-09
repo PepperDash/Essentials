e37fb33afe57c262721fcbf6b1ab54f07cb680d3
Andrew Welker <andrew.welker@gmail.com>
2020-11-09 12:10:44 -0700

    add DeviceInfo class

--- essentials-framework/Essentials Core/PepperDashEssentialsBase/Device Info/DeviceInfo.cs
+++ essentials-framework/Essentials Core/PepperDashEssentialsBase/Device Info/DeviceInfo.cs
@@ -2,6 +2,10 @@
 {
     public class DeviceInfo
     {
-         
+        public string HostName { get; set; } 
+        public string IpAddress { get; set; }
+        public string MacAddress { get; set; }
+        public string SerialNumber { get; set; }
+        public string FirmwareVersion { get; set; }
     }
 }
