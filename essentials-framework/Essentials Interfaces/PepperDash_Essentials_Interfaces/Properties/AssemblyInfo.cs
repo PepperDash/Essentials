13cbeb7605261a534ce93c3680aabb9fa9bb2b55
Andrew Welker <andrew.welker@gmail.com>
2020-11-24 16:09:30 -0700

    Add PepperDash_Essentials_Interfaces project

---
+++ essentials-framework/Essentials Interfaces/PepperDash_Essentials_Interfaces/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("PepperDash_Essentials_Interfaces")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PepperDash_Essentials_Interfaces")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
