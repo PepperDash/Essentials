ab6d44e60461497e9ec29c1b178aa986fb220feb
Andrew Welker <andrew.welker@gmail.com>
2020-12-07 11:48:48 -0700

    added enable property to ScheduledEventConfig

--- essentials-framework/Essentials Core/PepperDashEssentialsBase/Room/Config/EssentialsRoomScheduledEventsConfig.cs
+++ essentials-framework/Essentials Core/PepperDashEssentialsBase/Room/Config/EssentialsRoomScheduledEventsConfig.cs
@@ -33,5 +33,8 @@ namespace PepperDash.Essentials.Room.Config
 
         [JsonProperty("acknowledgeable")]
         public bool Acknowledgeable;
+
+        [JsonProperty("enable")]
+        public bool Enable;
     }
 }
