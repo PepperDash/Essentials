fc91ba7c1e3a57cd9e01a9a9727f158632c1c4c8
Jason Alborough <jalborough@pepperdash.com>
2020-12-16 15:00:30 -0500

    Fixes Namespace and Implements IKeyed in both interfaces.

--- essentials-framework/Essentials Core/PepperDashEssentialsBase/Interfaces/ILogStrings.cs
+++ essentials-framework/Essentials Core/PepperDashEssentialsBase/Interfaces/ILogStrings.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Crestron.SimplSharp;
-using PepperDash.Core.IKeyed;
+using PepperDash.Core;
 
-namespace PepperDash_Essentials_Core.Interfaces
+namespace PepperDash.Essentials.Core.Interfaces
 {
 	public interface ILogStrings : IKeyed
 	{
--- essentials-framework/Essentials Core/PepperDashEssentialsBase/Interfaces/ILogStringsWithLevel.cs
+++ essentials-framework/Essentials Core/PepperDashEssentialsBase/Interfaces/ILogStringsWithLevel.cs
@@ -5,9 +5,9 @@ using System.Text;
 using Crestron.SimplSharp;
 using PepperDash.Core;
 
-namespace PepperDash_Essentials_Core.Interfaces
+namespace PepperDash.Essentials.Core.Interfaces
 {
-	public interface ILogStringsWithLevel
+	public interface ILogStringsWithLevel : IKeyed
 	{
 		/// <summary>
 		/// Defines a class that is capable of logging a string with an int level
