1cad1976eedb719fe926318f911f317eac0b686d
Jason Alborough <jalborough@pepperdash.com>
2020-12-16 14:39:53 -0500

    #499 Adds interfaces ILogStrings and ILogStringsWithLevel to PepperDash_Essentials_Core.Interfaces

---
+++ essentials-framework/Essentials Core/PepperDashEssentialsBase/Interfaces/ILogStringsWithLevel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+using PepperDash.Core;
+
+namespace PepperDash_Essentials_Core.Interfaces
+{
+	public interface ILogStringsWithLevel
+	{
+		/// <summary>
+		/// Defines a class that is capable of logging a string with an int level
+		/// </summary>
+		void SendToLog(IKeyed device, Debug.ErrorLogLevel level,string logMessage);
+	}
+
+}
