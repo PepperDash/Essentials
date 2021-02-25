9f840fae416ba231c7c10c1c6f894785e381c33b
Andrew Welker <andrew.welker@gmail.com>
2021-02-25 16:53:35 -0700

    add DestinationListItem

---
+++ essentials-framework/Essentials Core/PepperDashEssentialsBase/Devices/DestinationListItem.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using PepperDash.Essentials.Core;
+
+namespace PepperDash.Essentials.Core
+{
+    public class DestinationListItem
+    {
+        [JsonProperty("sinkKey")]
+        public string SinkKey { get; set; }
+
+        private EssentialsDevice _sinkDevice;
+
+        [JsonIgnore]
+        public EssentialsDevice SinkDevice
+        {
+            get { return _sinkDevice ?? (_sinkDevice = DeviceManager.GetDeviceForKey(SinkKey) as EssentialsDevice); }
+        }
+
+        public string PreferredName
+        {
+            get
+            {
+                if (!string.IsNullOrEmpty(Name))
+                {
+                    return Name;
+                }
+
+                return SinkDevice == null ? "---" : SinkDevice.Name;
+            }
+        }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("includeInDestinationList")]
+        public bool IncludeInDestinationList { get; set; }
+
+        [JsonProperty("order")]
+        public int Order { get; set; }
+
+        [JsonProperty("surfaceLocation")]
+        public int SurfaceLocation { get; set; }
+
+        [JsonProperty("verticalLocation")]
+        public int VerticalLocation { get; set; }
+        
+        [JsonProperty("horizontalLocation")]
+        public int HorizontalLocation { get; set; }
+    }
+}
