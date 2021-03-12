4a9b1514e3f60adc7f2c82e52f24baab954d591c
Andrew Welker <andrew.welker@gmail.com>
2021-03-12 15:46:08 -0700

    fix old/wrong namespace versions

--- essentials-framework/Essentials Core/PepperDashEssentialsBase/DeviceTypeInterfaces/ILanguageDefinition.cs
+++ essentials-framework/Essentials Core/PepperDashEssentialsBase/DeviceTypeInterfaces/ILanguageDefinition.cs
@@ -20,7 +20,16 @@ namespace PepperDash.Essentials.Core.DeviceTypeInterfaces
 namespace PepperDash_Essentials_Core.DeviceTypeInterfaces
 {
     [Obsolete("Use PepperDash.Essentials.Core.DeviceTypeInterfaces")]
-    public interface ILanguageDefinition:PepperDash.Essentials.Core.DeviceTypeInterfaces.ILanguageDefinition
+    public interface ILanguageDefinition
     {
+        string LocaleName { get; set; }
+        string FriendlyName { get; set; }
+        bool Enable { get; set; }
+        List<LanguageLabel> UiLabels { get; set; }
+        List<LanguageLabel> Sources { get; set; }
+        List<LanguageLabel> Destinations { get; set; }
+        List<LanguageLabel> SourceGroupNames { get; set; }
+        List<LanguageLabel> DestinationGroupNames { get; set; }
+        List<LanguageLabel> RoomNames { get; set; }
     }
 }
--- essentials-framework/Essentials Core/PepperDashEssentialsBase/Queues/ComsMessage.cs
+++ essentials-framework/Essentials Core/PepperDashEssentialsBase/Queues/ComsMessage.cs
@@ -78,15 +78,23 @@ namespace PepperDash_Essentials_Core.Queues
     /// IBasicCommunication Message for IQueue
     /// </summary>
     [Obsolete("Use PepperDash.Essentials.Core.Queues")]
-    public class ComsMessage : PepperDash.Essentials.Core.Queues.ComsMessage
+    public class ComsMessage : IQueueMessage
     {
+        private readonly byte[] _bytes;
+        private readonly IBasicCommunication _coms;
+        private readonly string _string;
+        private readonly bool _isByteMessage;
+
         /// <summary>
         /// Constructor for a string message
         /// </summary>
         /// <param name="coms">IBasicCommunication to send the message</param>
         /// <param name="message">Message to send</param>
-        public ComsMessage(IBasicCommunication coms, string message):base(coms, message)
+        public ComsMessage(IBasicCommunication coms, string message)
         {
+            Validate(coms, message);
+            _coms = coms;
+            _string = message;
         }
 
         /// <summary>
@@ -94,8 +102,44 @@ namespace PepperDash_Essentials_Core.Queues
         /// </summary>
         /// <param name="coms">IBasicCommunication to send the message</param>
         /// <param name="message">Message to send</param>
-        public ComsMessage(IBasicCommunication coms, byte[] message):base(coms, message)
+        public ComsMessage(IBasicCommunication coms, byte[] message)
         {
+            Validate(coms, message);
+            _coms = coms;
+            _bytes = message;
+            _isByteMessage = true;
+        }
+
+        private void Validate(IBasicCommunication coms, object message)
+        {
+            if (coms == null)
+                throw new ArgumentNullException("coms");
+
+            if (message == null)
+                throw new ArgumentNullException("message");
+        }
+
+        /// <summary>
+        /// Dispatchs the string/byte[] to the IBasicCommunication specified
+        /// </summary>
+        public void Dispatch()
+        {
+            if (_isByteMessage)
+            {
+                _coms.SendBytes(_bytes);
+            }
+            else
+            {
+                _coms.SendText(_string);
+            }
+        }
+
+        /// <summary>
+        /// Shows either the byte[] or string to be sent
+        /// </summary>
+        public override string ToString()
+        {
+            return _bytes != null ? _bytes.ToString() : _string;
         }
     }
 }
