5171385b5bd5ffbf465dbe8677f4d4e8eeb93184
Neil Dorin <ndorin@pepperdash.com>
2021-11-05 15:58:10 -0600

    feat(essentials): adds JSON property attributes as needed

--- essentials-framework/Essentials Devices Common/Essentials Devices Common/VideoCodec/Interfaces/IHasCodecLayouts.cs
+++ essentials-framework/Essentials Devices Common/Essentials Devices Common/VideoCodec/Interfaces/IHasCodecLayouts.cs
@@ -6,6 +6,8 @@ using Crestron.SimplSharp;
 
 using PepperDash.Essentials.Core;
 
+using Newtonsoft.Json;
+
 namespace PepperDash.Essentials.Devices.Common.VideoCodec
 {
     /// <summary>
@@ -45,11 +47,17 @@ namespace PepperDash.Essentials.Devices.Common.VideoCodec
 
     public class LayoutInfoChangedEventArgs : EventArgs
     {
+        [JsonProperty("availableLayouts", NullValueHandling = NullValueHandling.Ignore)]
         public ZoomRoom.zConfiguration.eLayoutStyle AvailableLayouts { get; set; }
+        [JsonProperty("currentSelectedLayout", NullValueHandling = NullValueHandling.Ignore)]
         public ZoomRoom.zConfiguration.eLayoutStyle CurrentSelectedLayout { get; set; }
+        [JsonProperty("canSwapContentWithThumbnail", NullValueHandling = NullValueHandling.Ignore)]
         public bool CanSwapContentWithThumbnail { get; set; }
+        [JsonProperty("contentSwappedWithThumbnail", NullValueHandling = NullValueHandling.Ignore)]
         public bool ContentSwappedWithThumbnail { get; set; }
+        [JsonProperty("layoutViewIsOnFirstPage", NullValueHandling = NullValueHandling.Ignore)]
         public bool LayoutViewIsOnFirstPage { get; set; }
+        [JsonProperty("layoutViewIsOnLastPage", NullValueHandling = NullValueHandling.Ignore)]
         public bool LayoutViewIsOnLastPage { get; set; }
     }
 }
