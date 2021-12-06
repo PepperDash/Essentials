6bc966a869188938b483ba6d636e5af6aabdde86
Neil Dorin <ndorin@pepperdash.com>
2021-12-06 16:24:16 -0700

    feat(essentials): adds json attributes to LightingScene

--- essentials-framework/Essentials Core/PepperDashEssentialsBase/Lighting/LightingBase.cs
+++ essentials-framework/Essentials Core/PepperDashEssentialsBase/Lighting/LightingBase.cs
@@ -113,9 +113,12 @@ namespace PepperDash.Essentials.Core.Lighting
 
     public class LightingScene
     {
+        [JsonProperty("name", NullValueHandling=NullValueHandling.Ignore)]
         public string Name { get; set; }
+        [JsonProperty("id", NullValueHandling = NullValueHandling.Ignore)]
         public string ID { get; set; }
         bool _IsActive;
+        [JsonProperty("isActive", NullValueHandling = NullValueHandling.Ignore)]
         public bool IsActive 
         {
             get
@@ -128,6 +131,7 @@ namespace PepperDash.Essentials.Core.Lighting
                 IsActiveFeedback.FireUpdate();
             }
         }
+        [JsonIgnore]
         public BoolFeedback IsActiveFeedback { get; set; }
 
         public LightingScene()
