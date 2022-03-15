acfbefca00b9ed1e88deb26d371437bb13327850
Jason Alborough <jalborough@pepperdash.com>
2022-03-15 09:33:06 -0400

    feature: add static fileio class to read and write files

---
+++ essentials-framework/Essentials Core/PepperDashEssentialsBase/File/FileIO.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Crestron.SimplSharp;
+using Crestron.SimplSharp.CrestronIO;
+using PepperDash.Core;
+
+namespace PepperDash.Essentials.Core
+{
+	public static class FileIO
+	{
+
+		static CCriticalSection fileLock = new CCriticalSection();
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="fileName"></param>
+		/// <returns></returns>
+		public static bool FileExsists(string fileName)
+		{
+			if (GetFile(fileName) == null)
+			{
+				return false;
+			}
+			else
+			{
+				return true;
+			}
+		}
+		/// <summary>
+		/// Get the full file info from a path/filename, can include wildcards.
+		/// </summary>
+		/// <param name="fileName"></param>
+		/// <returns></returns>
+		public static FileInfo[] GetFiles(string fileName)
+		{
+			DirectoryInfo dirInfo = new DirectoryInfo(Path.GetDirectoryName(fileName));
+			var files = dirInfo.GetFiles(Path.GetFileName(fileName));
+			Debug.Console(0, "FileIO found: {0}, {1}", files.Count(), fileName);
+			if (files.Count() > 0)
+			{
+				return files;
+			}
+			else
+			{
+				return null;
+			}
+		}
+
+		public static FileInfo GetFile(string fileName)
+		{
+			DirectoryInfo dirInfo = new DirectoryInfo(Path.GetDirectoryName(fileName));
+			var files = dirInfo.GetFiles(Path.GetFileName(fileName));
+			Debug.Console(0, "FileIO found: {0}, {1}", files.Count(), fileName);
+			if (files.Count() > 0)
+			{
+				return files.FirstOrDefault();
+			}
+			else
+			{
+				return null;
+			}
+		}
+	
+
+		/// <summary>
+		/// Get the data from a fileName
+		/// </summary>
+		/// <param name="fileName"></param>
+		/// <returns></returns>
+		public static string GetDataFromFile(FileInfo file)
+		{
+			try
+			{
+				DirectoryInfo dirInfo = new DirectoryInfo(file.Name);
+				Debug.Console(2, "FileIO Getting Data {0}", file.FullName);
+
+				if (File.Exists(file.FullName))
+				{
+					using (StreamReader r = new StreamReader(file.FullName))
+					{
+						return r.ReadToEnd();
+					}
+				}
+				else
+				{
+					Debug.Console(2, "File {0} Does not exsist", file.FullName);
+					return "";
+				}
+				
+			}
+			catch (Exception e)
+			{
+				Debug.Console(0, Debug.ErrorLogLevel.Error, "Error: FileIO read failed: \r{0}", e);
+				return "";
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="data"></param>
+		/// <param name="filePath"></param>
+		public static void WriteDataToFile(string data, string filePath)
+		{
+			Debug.Console(0, Debug.ErrorLogLevel.Notice, "Attempting to write file: '{0}'", filePath);
+
+            try
+            {
+                if (fileLock.TryEnter())
+                {
+                    using (StreamWriter sw = new StreamWriter(filePath))
+                    {
+                        sw.Write(data);
+                        sw.Flush();
+                    }
+					
+                }
+                else
+                {
+                    Debug.Console(0, Debug.ErrorLogLevel.Error, "FileIO Unable to enter FileLock");
+                }
+
+            }
+            catch (Exception e)
+            {
+                Debug.Console(0, Debug.ErrorLogLevel.Error, "Error: FileIO write failed: \r{0}", e);
+            }
+            finally
+            {
+                if (fileLock != null && !fileLock.Disposed)
+                    fileLock.Leave();
+
+            }
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <returns></returns>
+		public static bool FileIoUnitTest()
+		{
+			var testData = "Testing FileIO";
+			FileIO.WriteDataToFile(testData, "\\USER\\FileIOTest.pdt");
+
+			var file = FileIO.GetFile("\\USER\\*FileIOTest*");
+			
+			var readData = FileIO.GetDataFromFile(file);
+			Debug.Console(0, "Returned {0}", readData);
+			File.Delete(file.FullName);
+			if (testData == readData)
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+
+	}
+}
