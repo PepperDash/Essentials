38c4a540a98ae2e5e5e870f00fb161fa8f3239fc
jta <jalborough@pepperdash.com>
2022-06-13 13:01:04 -0500

    fix for IBasicVolumeWithFeedback
    fix: for a change that split the interface but could break backwards compatibility #955

--- essentials-framework/Essentials Core/PepperDashEssentialsBase/Devices/IVolumeAndAudioInterfaces.cs
+++ essentials-framework/Essentials Core/PepperDashEssentialsBase/Devices/IVolumeAndAudioInterfaces.cs
@@ -55,8 +55,13 @@ namespace PepperDash.Essentials.Core
 	/// <summary>
 	/// Adds feedback and direct volume level set to IBasicVolumeControls
 	/// </summary>
-    public interface IBasicVolumeWithFeedback : IBasicVolumeControls, IHasVolumeControlWithFeedback, IHasMuteControlWithFeedback
+    public interface IBasicVolumeWithFeedback : IBasicVolumeControls
 	{
+		BoolFeedback MuteFeedback { get; }
+        void MuteOn();
+        void MuteOff();
+		void SetVolume(ushort level);
+		IntFeedback VolumeLevelFeedback { get; }
 	}
 
     /// <summary>
