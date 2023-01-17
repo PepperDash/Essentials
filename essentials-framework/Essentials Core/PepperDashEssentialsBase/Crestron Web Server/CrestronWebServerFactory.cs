6414631debcc762e26ecd59e2dbd74c8de160e5a
jdevito <jdevito@pepperdash.com>
2023-01-17 09:49:16 -0600

    feat: added crestron web server (cws) to Essentials_Core

---
+++ essentials-framework/Essentials Core/PepperDashEssentialsBase/Crestron Web Server/CrestronWebServerFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using PepperDash.Core;
+using PepperDash.Essentials.Core.Config;
+
+namespace PepperDash.Essentials.Core
+{
+	public class CrestronWebServerFactory : EssentialsDeviceFactory<CrestronWebServerBase>
+	{
+		public CrestronWebServerFactory()
+		{
+			TypeNames = new List<string> { "crestroncws", "cws" };
+		}
+		public override EssentialsDevice BuildDevice(DeviceConfig dc)
+		{
+			Debug.Console(1, "Factory Attempting to create new Crestron CWS Device");
+
+			return new CrestronWebServerBase(dc.Key, dc.Name, "");
+		}
+	}
+}
