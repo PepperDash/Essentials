759fb1d72949bd88e76cc071249740eb6399327e
jdevito <jdevito@pepperdash.com>
2023-09-06 11:22:19 -0500

    feature: adds support for HdSpXxx switchers
    WIP: added support for HD-PS401, HD-PS402, HD-PS621 and HD-PS622,
    pending testing with device.

---
+++ essentials-framework/Essentials DM/Essentials_DM/Config/HdPsXxxPropertiesConfig.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using PepperDash.Core;
+
+namespace PepperDash_Essentials_DM.Config
+{
+	public class HdPsXxxPropertiesConfig
+	{
+		[JsonProperty("control")]
+		public ControlPropertiesConfig Control { get; set; }
+
+		[JsonProperty("inputs")]
+		public Dictionary<uint, string> Inputs { get; set; } 
+
+		[JsonProperty("outputs")]
+		public Dictionary<uint, string> Outputs { get; set; } 
+	}
+}
