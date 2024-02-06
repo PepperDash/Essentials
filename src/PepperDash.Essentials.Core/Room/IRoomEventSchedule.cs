35e9e54564e480bb6b1eba5f77fe0df35f5d92ef
Andrew Welker <awelker@pepperdash.com>
2024-02-06 08:10:02 -0600

    feat: RKST-130 add `IRoomEventSchedule` interface

---
+++ src/PepperDash.Essentials.Core/Room/IRoomEventSchedule.cs
@@ -0,0 +1,12 @@
+﻿using PepperDash.Essentials.Room.Config;
+using System.Collections.Generic;
+
+namespace PepperDash.Essentials.Core.Room
+{
+    public interface IRoomEventSchedule
+    {
+        void AddOrUpdateScheduledEvent(ScheduledEventConfig eventConfig);
+
+        List<ScheduledEventConfig> GetScheduledEvents();
+    }
+}
