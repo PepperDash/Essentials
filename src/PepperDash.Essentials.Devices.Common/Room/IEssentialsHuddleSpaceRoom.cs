56280428418aadf5b9df2ff482aa01110c6e487a
Andrew Welker <awelker@pepperdash.com>
2024-02-06 10:51:25 -0600

    feat: move room aggregate interfaces back into Essentials

---
+++ src/PepperDash.Essentials.Devices.Common/Room/IEssentialsHuddleSpaceRoom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PepperDash.Essentials.Core;
+
+namespace PDT.Plugins.Essentials.Rooms
+{
+    public interface IEssentialsHuddleSpaceRoom : IEssentialsRoom, IHasCurrentSourceInfoChange, IRunRouteAction, IHasDefaultDisplay, IHasCurrentVolumeControls, IRoomOccupancy,
+        IEmergency, IMicrophonePrivacy
+    {
+        bool ExcludeFromGlobalFunctions { get; }
+
+        void RunRouteAction(string routeKey);
+
+        // EssentialsHuddleRoomPropertiesConfig PropertiesConfig { get; }
+
+        IBasicVolumeControls CurrentVolumeControls { get; }
+
+        event EventHandler<VolumeDeviceChangeEventArgs> CurrentVolumeDeviceChange;
+    }
+}
---
+++ src/PepperDash.Essentials.Devices.Common/Room/IEssentialsHuddleVtc1Room.cs
@@ -0,0 +1,25 @@
+﻿using PepperDash.Essentials.Core;
+using PepperDash.Essentials.Devices.Common.AudioCodec;
+using PepperDash.Essentials.Devices.Common.Codec;
+using PepperDash.Essentials.Devices.Common.VideoCodec;
+
+namespace PDT.Plugins.Essentials.Rooms
+{
+    public interface IEssentialsHuddleVtc1Room : IEssentialsRoom, IHasCurrentSourceInfoChange, IHasCurrentVolumeControls, IRunRouteAction, IRunDefaultCallRoute, IHasVideoCodec, IHasAudioCodec, IHasDefaultDisplay, IHasInCallFeedback,
+        IRoomOccupancy, IEmergency, IMicrophonePrivacy
+    {
+        // EssentialsHuddleVtc1PropertiesConfig PropertiesConfig { get; }
+
+        bool ExcludeFromGlobalFunctions { get; }
+
+        void RunRouteAction(string routeKey);
+
+        IHasScheduleAwareness ScheduleSource { get; }
+
+        new BoolFeedback InCallFeedback { get; }
+
+        new BoolFeedback PrivacyModeIsOnFeedback { get; }
+
+        string DefaultCodecRouteString { get; }
+    }
+}
