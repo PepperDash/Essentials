693d4f8723489fc94a23a2bda95923ac8c2e9a37
Andrew Welker <awelker@pepperdash.com>
2024-03-08 20:26:13 -0600

    feat: RKST-161 add IHasInputs & IInputs interfaces

---
+++ src/PepperDash.Essentials.Core/DeviceTypeInterfaces/IHasInputs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PepperDash.Essentials.Core.DeviceTypeInterfaces
+{
+    public interface IHasInputs
+    {
+        event EventHandler InputsUpdated;
+        IDictionary<string, IInput> Inputs { get; } 
+    }
+}
---
+++ src/PepperDash.Essentials.Core/DeviceTypeInterfaces/IInput.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using PepperDash.Core;
+
+namespace PepperDash.Essentials.Core.DeviceTypeInterfaces
+{
+    public interface IInput : IKeyName
+    {
+        event EventHandler InputUpdated;
+        bool IsSelected { get; }
+        void Select();
+    }
+}
