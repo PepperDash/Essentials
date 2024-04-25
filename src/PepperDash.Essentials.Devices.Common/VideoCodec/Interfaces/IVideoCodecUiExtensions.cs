d8e2f8cd51efc432e684a5801922a3d151a03d8c
Joshua_Gutenplan <18546589+jgutenplan@users.noreply.github.com>
2024-04-25 10:03:38 -0700

    feat: IVideoCodecUiExtensions

---
+++ src/PepperDash.Essentials.Devices.Common/VideoCodec/Interfaces/IVideoCodecUiExtensions.cs
@@ -0,0 +1,12 @@
+﻿namespace PepperDash.Essentials.Devices.Common.VideoCodec.Interfaces
+{
+    public interface IVideoCodecUiExtensionsHandler : IVideoCodecUiExtensionsWebViewDisplayAction, IVideoCodecUiExtensionsClickedEvent
+    {
+    }
+
+    public interface IVideoCodecUiExtensions
+    {
+        IVideoCodecUiExtensionsHandler VideoCodecUiExtensionsHandler { get; set; }
+    }
+
+}
---
+++ src/PepperDash.Essentials.Devices.Common/VideoCodec/Interfaces/IVideoCodecUiExtensionsClickedAction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PepperDash.Essentials.Devices.Common.VideoCodec.Interfaces
+{
+	public interface IVideoCodecUiExtensionsWebViewDisplayAction
+	{
+		Action<UiWebViewDisplayActionArgs> UiWebViewDisplayAction { get; set; }
+	}
+
+	public class UiWebViewDisplayActionArgs
+	{
+
+		/// <summary>
+		/// Required <0 - 2000>	The URL of the web page.
+		/// </summary>
+		public string Url { get; set; }
+
+		/// <summary>
+		/// Fullscreen, Modal Full screen: Display the web page on the entire screen.Modal: Display the web page in a window.
+		/// </summary>
+
+		public string Mode { get; set; }
+
+		/// <summary>
+		/// <0 - 255> The title of the web page.
+		/// </summary>
+		public string Title { get; set; }
+
+		/// <summary>
+		/// <0 - 8192> An HTTP header field.You can add up 15 Header parameters in one command, each holding one HTTP header field.
+		/// </summary>
+		public string Header { get; set; }
+	}
+
+}
