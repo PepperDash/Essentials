2b4dc8d4351e6a7c5e6cbbb61c8e55ba40971df0
Andrew Welker <awelker@pepperdash.com>
2024-05-08 08:37:25 -0500

    feat: add 2 new routing interfaces to allow for getting feedback for routing

---
+++ src/PepperDash.Essentials.Core/Routing/IRoutingWithFeedback.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System;
+
+namespace PepperDash.Essentials.Core
+{
+    /// <summary>
+    /// Defines an IRouting with a feedback event
+    /// </summary>
+    public interface IRoutingWithFeedback : IRouting
+    {
+        List<RouteSwitchDescriptor> CurrentRoutes { get; }
+
+        event EventHandler RoutingChanged;
+    }
+}
