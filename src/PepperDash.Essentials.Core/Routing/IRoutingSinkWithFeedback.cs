3823943cd9ec2a86673951ca5381f4add753f95c
Andrew Welker <awelker@pepperdash.com>
2024-05-08 08:37:25 -0500

    feat: add 2 new routing interfaces to allow for getting feedback for routing

---
+++ src/PepperDash.Essentials.Core/Routing/IRoutingSinkWithFeedback.cs
@@ -0,0 +1,26 @@
+﻿using PepperDash.Essentials.Core.Routing;
+using System;
+using System.Collections.Generic;
+
+namespace PepperDash.Essentials.Core
+{
+    /// <summary>
+    /// For fixed-source endpoint devices
+    /// </summary>
+    public interface IRoutingSinkWithFeedback : IRoutingSinkWithSwitching
+    {
+        RouteSwitchDescriptor CurrentRoute { get; }
+
+        event EventHandler InputChanged;
+    }
+
+/*    /// <summary>
+    /// For fixed-source endpoint devices
+    /// </summary>
+    public interface IRoutingSinkWithFeedback<TSelector> : IRoutingSinkWithSwitching<TSelector>
+    {
+        RouteSwitchDescriptor CurrentRoute { get; }
+
+        event EventHandler InputChanged;
+    }*/
+}
