c755ecb16cb9780860a21872330ff79a68e06fdd
Andrew Welker <awelker@pepperdash.com>
2024-05-28 11:26:25 -0500

    fix: add correct casing to base touchpanel config

--- src/PepperDash.Essentials.Core/Touchpanels/CrestronTouchpanelPropertiesConfig.cs
+++ src/PepperDash.Essentials.Core/Touchpanels/CrestronTouchpanelPropertiesConfig.cs
@@ -1,20 +1,49 @@
-﻿namespace PepperDash.Essentials.Core
+﻿using Newtonsoft.Json;
+
+namespace PepperDash.Essentials.Core
 {
     public class CrestronTouchpanelPropertiesConfig
     {
+        [JsonProperty("ipId")]
         public string IpId { get; set; }
+
+        [JsonProperty("defaultRoomKey")]
         public string DefaultRoomKey { get; set; }
+        
+        [JsonProperty("roomListKey")]
         public string RoomListKey { get; set; }
+
+        [JsonProperty("sgdFile")]
         public string SgdFile { get; set; }
+
+        [JsonProperty("projectName")]
         public string ProjectName { get; set; }
+
+        [JsonProperty("showVolumeGauge")]
         public bool ShowVolumeGauge { get; set; }
+
+        [JsonProperty("usesSplashPage")]
         public bool UsesSplashPage { get; set; }
+
+        [JsonProperty("showDate")]
         public bool ShowDate { get; set; }
+
+        [JsonProperty("showTime")]
         public bool ShowTime { get; set; }
+
+        [JsonProperty("setup")]
         public UiSetupPropertiesConfig Setup { get; set; }
+
+        [JsonProperty("headerStyle")]
         public string HeaderStyle { get; set; }
+
+        [JsonProperty("includeInFusionRoomHealth")]
         public bool IncludeInFusionRoomHealth { get; set; }
+
+        [JsonProperty("screenSaverTimeoutMin")]
         public uint ScreenSaverTimeoutMin { get; set; }
+
+        [JsonProperty("screenSaverMovePositionIntervalMs")]
         public uint ScreenSaverMovePositionIntervalMs { get; set; }
 
 
@@ -22,6 +51,7 @@
         /// The count of sources that will trigger the "additional" arrows to show on the SRL.
         /// Defaults to 5
         /// </summary>
+        [JsonProperty("sourcesOverflowCount")]
         public int SourcesOverflowCount { get; set; }
 
         public CrestronTouchpanelPropertiesConfig()
@@ -49,6 +79,7 @@
     /// </summary>
     public class UiSetupPropertiesConfig
     {
+        [JsonProperty("isVisible")]
         public bool IsVisible { get; set; }
     }
 }
