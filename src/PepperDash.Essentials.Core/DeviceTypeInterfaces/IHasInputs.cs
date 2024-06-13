3a56e47c4831a22f541992c96193613242cf5495
Neil Dorin <ndorin@pepperdash.com>
2024-06-13 10:59:05 -0600

    fix: updates IHasInputs to remove second generic that is unnecessary.

--- src/PepperDash.Essentials.Core/DeviceTypeInterfaces/IHasInputs.cs
+++ src/PepperDash.Essentials.Core/DeviceTypeInterfaces/IHasInputs.cs
@@ -15,7 +15,21 @@ namespace PepperDash.Essentials.Core.DeviceTypeInterfaces
     /// <example>
     /// See MockDisplay for example implemntation
     /// </example>
+    [Obsolete("Use IHasInputs<T> instead.  Will be removed for 2.0 release")]
     public interface IHasInputs<T, TSelector>: IKeyName
+    {
+        ISelectableItems<T> Inputs { get; }
+    }
+
+
+    /// <summary>
+    /// Describes a device that has selectable inputs
+    /// </summary>
+    /// <typeparam name="T">the type to use as the key for each input item. Most likely an enum or string</typeparam>\
+    /// <example>
+    /// See MockDisplay for example implemntation
+    /// </example>
+    public interface IHasInputs<T> : IKeyName
     {
         ISelectableItems<T> Inputs { get; }
     }
