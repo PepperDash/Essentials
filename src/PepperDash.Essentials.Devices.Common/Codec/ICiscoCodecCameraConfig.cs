d9721a362e8e5bbe18a7120b78065baf168ada6c
Neil Dorin <ndorin@pepperdash.com>
2024-06-28 08:55:18 -0600

    feat: adds method to set input source type and corresponding enum to ICiscoCodecCameraConfig

---
+++ src/PepperDash.Essentials.Devices.Common/Codec/ICiscoCodecCameraConfig.cs
@@ -0,0 +1,31 @@
+﻿using Crestron.SimplSharpPro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PepperDash.Essentials.Devices.Common.Codec
+{ 
+    /// <summary>
+    /// Describes a cisco codec device that can allow configuration of cameras
+    /// </summary>
+    public interface ICiscoCodecCameraConfig
+    {
+        void SetCameraAssignedSerialNumber(uint cameraId, string serialNumber);
+
+        void SetCameraName(uint videoConnecterId, string name);
+
+        void SetInputSourceType(uint videoConnectorId, eCiscoCodecInputSourceType sourceType);
+    }
+
+    public enum eCiscoCodecInputSourceType
+    {
+        PC,
+        camera,
+        document_camera,
+        mediaplayer,
+        other,
+        whiteboard
+    }
+}
