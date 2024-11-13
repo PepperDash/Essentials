0b60f53d0edd25b8dd19e765c421f9fd0683bf85
Neil Dorin <ndorin@pepperdash.com>
2024-11-13 12:15:25 -0700

    feat: Adds IEssentialsRoomEmergency interface and implements on contact closure device to provide state

--- src/PepperDash.Essentials.Core/Room/EsentialsRoomEmergencyContactClosure.cs
+++ src/PepperDash.Essentials.Core/Room/EsentialsRoomEmergencyContactClosure.cs
@@ -4,11 +4,15 @@ using PepperDash.Essentials.Room.Config;
 
 namespace PepperDash.Essentials.Core
 {
-    public class EssentialsRoomEmergencyContactClosure : EssentialsRoomEmergencyBase
+    public class EssentialsRoomEmergencyContactClosure : EssentialsRoomEmergencyBase, IEssentialsRoomEmergency
     {
+        public event EventHandler<EventArgs> EmergencyStateChange;
+
         IEssentialsRoom Room;
         string Behavior;
         bool TriggerOnClose;
+
+        public bool InEmergency { get; private set; }
 
         public EssentialsRoomEmergencyContactClosure(string key, EssentialsRoomEmergencyConfig config, IEssentialsRoom room) :
             base(key)
@@ -32,7 +36,18 @@ namespace PepperDash.Essentials.Core
         void EsentialsRoomEmergencyContactClosure_StateChange(DigitalInput digitalInput, DigitalInputEventArgs args)
         {
             if (args.State && TriggerOnClose || !args.State && !TriggerOnClose)
+            {
+                InEmergency = true;
+                if (EmergencyStateChange != null)
+                    EmergencyStateChange(this, new EventArgs());
                 RunEmergencyBehavior();
+            }
+            else
+            {
+                InEmergency = false;
+                if (EmergencyStateChange != null)
+                    EmergencyStateChange(this, new EventArgs());
+            }
         }
 
         /// <summary>
@@ -43,5 +58,15 @@ namespace PepperDash.Essentials.Core
             if (Behavior.Equals("shutdown"))
                 Room.Shutdown();
         }
+    }
+
+    /// <summary>
+    /// Describes the functionality of a room emergency contact closure
+    /// </summary>
+    public interface IEssentialsRoomEmergency
+    {
+        event EventHandler<EventArgs> EmergencyStateChange;
+
+        bool InEmergency { get; }
     }
 }
