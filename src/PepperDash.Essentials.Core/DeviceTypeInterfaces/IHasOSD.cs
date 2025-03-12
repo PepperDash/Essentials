67a7422cd0437a2a7b0a411f320dafd252a39dc0
aknous <aknous@pepperdash.com>
2025-03-11 23:33:59 -0400

    feat: adds IHasOSD interface

---
+++ src/PepperDash.Essentials.Core/DeviceTypeInterfaces/IHasOSD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PepperDash.Essentials.Core.DeviceTypeInterfaces
+{
+    public interface IHasOSD
+    {
+        void ShowOsdMessage(string url, string mobileControlPath, string mode, string title, string target);
+        void HideOsdMessage();
+    }
+}
