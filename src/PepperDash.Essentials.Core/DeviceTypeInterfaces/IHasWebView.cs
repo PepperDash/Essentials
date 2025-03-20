8762f84548f8d3957b5c134896aac4d001c9bb3a
aknous <aknous@pepperdash.com>
2025-03-20 00:40:39 -0400

    feat: adds WebviewVisible property to IHasWebView

--- src/PepperDash.Essentials.Core/DeviceTypeInterfaces/IHasWebView.cs
+++ src/PepperDash.Essentials.Core/DeviceTypeInterfaces/IHasWebView.cs
@@ -8,6 +8,7 @@ namespace PepperDash.Essentials.Core.DeviceTypeInterfaces
 {
     public interface IHasWebView
     {
+        public bool WebviewIsVisible { get; }
         void ShowWebView(string url, string mode, string title, string target);
         void HideWebView();
     }
