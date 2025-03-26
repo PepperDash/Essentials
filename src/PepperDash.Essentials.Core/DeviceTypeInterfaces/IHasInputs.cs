90aa4a5d6261ea34c2a35233dbb9f7b569148375
Andrew Welker <awelker@peppedash.com>
2025-03-26 08:55:55 -0500

    feat: remove IHasInputs<T,R> interface
    This has been replaced by the `IHasInputs<T>` interface and was marked to be removed in the 2.0.0 release.

--- src/PepperDash.Essentials.Core/DeviceTypeInterfaces/IHasInputs.cs
+++ src/PepperDash.Essentials.Core/DeviceTypeInterfaces/IHasInputs.cs
@@ -1,27 +1,7 @@
 ﻿using PepperDash.Core;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PepperDash.Essentials.Core.DeviceTypeInterfaces
 {
-
-    /// <summary>
-    /// Describes a device that has selectable inputs
-    /// </summary>
-    /// <typeparam name="TKey">the type to use as the key for each input item. Most likely an enum or string</typeparam>\
-    /// <example>
-    /// See MockDisplay for example implemntation
-    /// </example>
-    [Obsolete("Use IHasInputs<T> instead.  Will be removed for 2.0 release")]
-    public interface IHasInputs<T, TSelector>: IKeyName
-    {
-        ISelectableItems<T> Inputs { get; }
-    }
-
-
     /// <summary>
     /// Describes a device that has selectable inputs
     /// </summary>
