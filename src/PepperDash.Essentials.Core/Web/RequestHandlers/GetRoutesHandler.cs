bb045ba06b321fff8f43451e9b5185d7caee73da
Andrew Welker <awelker@peppedash.com>
2025-03-28 10:32:16 -0500

    fix: add base URL to routes response

--- src/PepperDash.Essentials.Core/Web/RequestHandlers/GetRoutesHandler.cs
+++ src/PepperDash.Essentials.Core/Web/RequestHandlers/GetRoutesHandler.cs
@@ -1,4 +1,5 @@
-﻿using Crestron.SimplSharp.WebScripting;
+﻿using Crestron.SimplSharp;
+using Crestron.SimplSharp.WebScripting;
 using Newtonsoft.Json;
 using PepperDash.Core.Web.RequestHandlers;
 
@@ -7,13 +8,30 @@ namespace PepperDash.Essentials.Core.Web.RequestHandlers
     public class GetRoutesHandler:WebApiBaseRequestHandler
     {
         private HttpCwsRouteCollection routeCollection;
-        public GetRoutesHandler(HttpCwsRouteCollection routeCollection) {
+        private string basePath;
+
+        public GetRoutesHandler(HttpCwsRouteCollection routeCollection, string basePath) {
             this.routeCollection = routeCollection;
+            this.basePath = basePath;
         }
 
         protected override void HandleGet(HttpCwsContext context)
         {
-            var response = JsonConvert.SerializeObject(routeCollection);
+            var currentIp = CrestronEthernetHelper.GetEthernetParameter(
+                CrestronEthernetHelper.ETHERNET_PARAMETER_TO_GET.GET_CURRENT_IP_ADDRESS, 0);
+
+            var hostname = CrestronEthernetHelper.GetEthernetParameter(
+                    CrestronEthernetHelper.ETHERNET_PARAMETER_TO_GET.GET_HOSTNAME, 0);
+
+            var path = CrestronEnvironment.DevicePlatform == eDevicePlatform.Server
+                ? $"https://{hostname}/VirtualControl/Rooms/{InitialParametersClass.RoomId}/cws{basePath}"
+                : $"https://{currentIp}/cws{basePath}";
+
+            var response = JsonConvert.SerializeObject(new RoutesResponseObject()
+            {
+                Url = path,
+                Routes = routeCollection
+            });
 
             context.Response.StatusCode = 200;
             context.Response.ContentType = "application/json";
@@ -21,5 +39,14 @@ namespace PepperDash.Essentials.Core.Web.RequestHandlers
             context.Response.Write(response, false);
             context.Response.End();
         }
+    }
+
+    public class  RoutesResponseObject 
+    {
+        [JsonProperty("url")]
+        public string Url { set; get; }
+
+        [JsonProperty("routes")]
+        public HttpCwsRouteCollection Routes { get; set; }
     }
 }
