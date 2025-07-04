9febbf2557121cd8e8627015601400f0659d70cc
Andrew Welker <awelker@pepperdash.com>
2025-07-04 12:46:08 -0500

    chore: fix issues related to remving crestron usings

--- src/PepperDash.Core/JsonToSimpl/JsonToSimplMaster.cs
+++ src/PepperDash.Core/JsonToSimpl/JsonToSimplMaster.cs
@@ -167,11 +167,7 @@ namespace PepperDash.Core.JsonToSimpl
             /// <returns></returns>
 			public static JObject ParseObject(string json)
 			{
-				#if NET6_0
                 using (var reader = new JsonTextReader(new System.IO.StringReader(json)))
-#else
-                using (var reader = new JsonTextReader(new System.IO.StringReader(json)))
-#endif
 				{
 					var startDepth = reader.Depth;
 					var obj = JObject.Load(reader);
@@ -191,11 +187,8 @@ namespace PepperDash.Core.JsonToSimpl
    /// </summary>
 			public static JArray ParseArray(string json)
 			{
-				#if NET6_0
+
                 using (var reader = new JsonTextReader(new System.IO.StringReader(json)))
-#else
-                using (var reader = new JsonTextReader(new System.IO.StringReader(json)))
-#endif
 				{
 					var startDepth = reader.Depth;
 					var obj = JArray.Load(reader);
