2eb9442c0a62036967fd2622e13713737d150dac
Aviv Cohn <acohn@pepperdash.com>
2025-12-09 22:27:07 -0500

    feat: remove unused imports, seperate StatusProperties

---
+++ src/PepperDash.Essentials.Core/UDMApi/StatusProperties.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PepperDash.Essentials.Core.UDMApi
+{
+    internal class StatusProperties
+    {
+        /// <summary>
+        /// Dictionary of device statuses keyed by device identifier
+        /// </summary>
+        [JsonProperty("devices")]
+        public Dictionary<string, DeviceStatus> devices { get; set; }
+
+        public StatusProperties()
+        {
+            devices = new Dictionary<string, DeviceStatus>();
+        }
+    }
+}
