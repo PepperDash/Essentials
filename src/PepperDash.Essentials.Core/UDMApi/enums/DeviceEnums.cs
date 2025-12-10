c66062d87fe56382007b0b20c0fbf5d1f3647f8e
Aviv Cohn <acohn@pepperdash.com>
2025-12-10 09:24:04 -0500

    feat: add device enums

---
+++ src/PepperDash.Essentials.Core/UDMApi/enums/DeviceEnums.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PepperDash.Essentials.Core
+{
+    internal class DeviceEnums
+    {
+        public enum DeviceKeys
+        {
+            device1,
+            device2,
+            device3,
+            device4,
+            device5,
+            device6,
+            device7,
+            device8,
+            device9,
+            device10,
+            device11,
+            device12,
+            device13,
+            device14,
+            device15,
+            device16,
+            device17,
+            device18,
+            device19,
+            device20
+
+        }
+    }
+}
