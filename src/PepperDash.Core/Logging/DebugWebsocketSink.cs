b1a575e4d2dc6063e58900294af3d3ecbc399f1a
Neil Dorin <ndorin@pepperdash.com>
2026-05-01 14:43:48 -0600

    feat: enhance URL generation in DebugWebsocketSink to support dual-stack environments

--- src/PepperDash.Core/Logging/DebugWebsocketSink.cs
+++ src/PepperDash.Core/Logging/DebugWebsocketSink.cs
@@ -46,8 +46,15 @@ namespace PepperDash.Core
         {
             get
             {
-                if (_httpsServer == null) return "";
-                return $"wss://{CrestronEthernetHelper.GetEthernetParameter(CrestronEthernetHelper.ETHERNET_PARAMETER_TO_GET.GET_CURRENT_IP_ADDRESS, 0)}:{_httpsServer.Port}{_httpsServer.WebSocketServices[_path].Path}";
+                if (_httpsServer == null || !_httpsServer.IsListening) return "";
+                var service = _httpsServer.WebSocketServices[_path];
+                if (service == null) return "";
+
+                // Use CSLAN IP if available, otherwise fallback to primary IP. This ensures we provide a reachable URL in dual-stack environments.
+                if (!string.IsNullOrEmpty(CrestronEthernetHelper.GetEthernetParameter(CrestronEthernetHelper.ETHERNET_PARAMETER_TO_GET.GET_CURRENT_IP_ADDRESS, 1)))
+                    return $"wss://{CrestronEthernetHelper.GetEthernetParameter(CrestronEthernetHelper.ETHERNET_PARAMETER_TO_GET.GET_CURRENT_IP_ADDRESS, 1)}:{_httpsServer.Port}{service.Path}";
+                else
+                    return $"wss://{CrestronEthernetHelper.GetEthernetParameter(CrestronEthernetHelper.ETHERNET_PARAMETER_TO_GET.GET_CURRENT_IP_ADDRESS, 0)}:{_httpsServer.Port}{service.Path}";
             }
         }
 
