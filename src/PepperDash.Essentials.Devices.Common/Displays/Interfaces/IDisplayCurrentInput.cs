ff1393b768cd3211c24faf5372960e5cc311bffd
Neil Dorin <ndorin@pepperdash.com>
2026-05-06 17:13:48 -0600

    refactor: Simplify namespace declaration and formatting in IDisplayCurrentInput interface

--- src/PepperDash.Essentials.Devices.Common/Displays/Interfaces/IDisplayCurrentInput.cs
+++ src/PepperDash.Essentials.Devices.Common/Displays/Interfaces/IDisplayCurrentInput.cs
@@ -1,11 +1,11 @@
 using PepperDash.Core;
 
-namespace PepperDash.Essentials.Core.DeviceTypeInterfaces
-{
-    /// <summary>
-    /// Defines the contract for a display device that has current input information. This is used to provide a common interface for the TwoWayDisplayBaseMessenger to get current input information without requiring the full TwoWayDisplayBase class
-    /// </summary>
-    public interface IDisplayCurrentInput : IKeyName
+namespace PepperDash.Essentials.Core.DeviceTypeInterfaces;
+
+/// <summary>
+/// Defines the contract for a display device that has current input information. This is used to provide a common interface for the TwoWayDisplayBaseMessenger to get current input information without requiring the full TwoWayDisplayBase class
+/// </summary>
+public interface IDisplayCurrentInput : IKeyName
 {
     /// <summary>
     /// Gets the Current Input feedback for the display device.
