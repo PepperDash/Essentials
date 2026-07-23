a782651e4d950cb3e26d340aa551e6131ee4cd6a
Neil Dorin <ndorin@pepperdash.com>
2026-07-23 08:49:11 -0600

    feat: enhance documentation for IDtmfDecode interface and DtmfReceivedEventArgs class

--- src/PepperDash.Essentials.Devices.Common/AudioCodec/Interfaces/IDtmfDecode.cs
+++ src/PepperDash.Essentials.Devices.Common/AudioCodec/Interfaces/IDtmfDecode.cs
@@ -1,5 +1,8 @@
 namespace PepperDash.Essentials.Devices.Common.AudioCodec;
 
+/// <summary>
+/// Interface for devices that can decode DTMF digits
+/// </summary>
 public interface IDtmfDecode
 {
     /// <summary>
@@ -8,10 +11,20 @@ public interface IDtmfDecode
     event EventHandler<DtmfReceivedEventArgs> DtmfReceived;
 }
 
+/// <summary>
+/// Event args for DTMF received event
+/// </summary>
 public class DtmfReceivedEventArgs : EventArgs
 {
+    /// <summary>
+    /// The DTMF digit that was received
+    /// </summary>
     public string Digit { get; private set; }
 
+    /// <summary>
+    /// Event args for DTMF received event
+    /// </summary>
+    /// <param name="digit">The DTMF digit that was received</param>
     public DtmfReceivedEventArgs(string digit)
     {
         Digit = digit;
